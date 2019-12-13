2a8ffbb55c9a39129b4e86d40a6535a88cdd9952
Gabriel Brun <lydstyl@gmail.com>
2019-12-13 22:22:07 +0100

    refacto

--- index.js
+++ index.js
@@ -1,40 +1,32 @@
-const PizZip = require('pizzip');
-const Docxtemplater = require('docxtemplater');
-
 const fs = require('fs');
 const path = require('path');
 const yamlFront = require('yaml-front-matter');
-
 const dateFormat = require('dateformat');
+const PizZip = require('pizzip');
+const Docxtemplater = require('docxtemplater');
 
 function compare(a, b) {
   if (a.begin < b.begin) return 1;
   return -1;
 }
 
-fs.readFile(
-  '/home/lyd/APPS-SCRIPTS/190907-developpeur-react-nord/src/markdown-pages/cv.md',
-  'utf8',
-  (err, data) => {
+function makeResume(markdownCV, input, output, anonyme = false) {
+  fs.readFile(markdownCV, 'utf8', (err, data) => {
     if (err) throw err;
 
-    //NORMAL RESUME
-    const content = fs.readFileSync(
-      path.resolve(__dirname, 'input.docx'),
-      'binary'
-    );
-
+    const content = fs.readFileSync(path.resolve(__dirname, input), 'binary');
     const zip = new PizZip(content);
-
     const doc = new Docxtemplater();
     doc.loadZip(zip);
 
-    let { description, objectif, skills, experience } = yamlFront.loadFront(
+    const { description, objectif, skills, experience } = yamlFront.loadFront(
       data
     );
 
     doc.setData({
-      description,
+      description: anonyme
+        ? description.replace('Gabriel Brun', '<nom anonyme>')
+        : description,
       objectif,
       mainSkills: skills.main
         .map(skill => `${skill.title}: ${skill.rate}/10`)
@@ -44,43 +36,7 @@ fs.readFile(
         return {
           begin: dateFormat(xp.begin, 'mm/yyyy'),
           end: dateFormat(xp.end, 'mm/yyyy'),
-          company: xp.company,
-          job: xp.job,
-          tasks: xp.body.split('\n').map(task => {
-            return { task };
-          })
-        };
-      })
-    });
-
-    //ANONYME RESUME
-    const anonymeContent = fs.readFileSync(
-      path.resolve(__dirname, 'anonymeTemplate.docx'),
-      'binary'
-    );
-
-    const anonymeZip = new PizZip(anonymeContent);
-
-    const anonymeDoc = new Docxtemplater();
-    anonymeDoc.loadZip(anonymeZip);
-
-    description = yamlFront.loadFront(data).description;
-    objectif = yamlFront.loadFront(data).objectif;
-    skills = yamlFront.loadFront(data).skills;
-    experience = yamlFront.loadFront(data).experience;
-
-    anonymeDoc.setData({
-      description: description.replace('Gabriel Brun', 'X'),
-      objectif,
-      mainSkills: skills.main
-        .map(skill => `${skill.title}: ${skill.rate}/10`)
-        .join(', '),
-      otherSkills: skills.other.map(skill => `${skill.title}`).join(', '),
-      experiences: experience.sort(compare).map(xp => {
-        return {
-          begin: dateFormat(xp.begin, 'mm/yyyy'),
-          end: dateFormat(xp.end, 'mm/yyyy'),
-          company: 'Entreprise X',
+          company: anonyme ? '<entreprise anonyme>' : xp.company,
           job: xp.job,
           tasks: xp.body.split('\n').map(task => {
             return { task };
@@ -90,9 +46,7 @@ fs.readFile(
     });
 
     try {
-      // replace all occurences in document
-      doc.render();
-      anonymeDoc.render();
+      doc.render(); // replace all occurences in document
     } catch (error) {
       const e = {
         message: error.message,
@@ -106,10 +60,14 @@ fs.readFile(
     }
 
     const buf = doc.getZip().generate({ type: 'nodebuffer' });
-    const buf2 = anonymeDoc.getZip().generate({ type: 'nodebuffer' });
-
     // buf is a nodejs buffer, you can either write it to a file or do anything else with it.
-    fs.writeFileSync(path.resolve(__dirname, 'CV_Gabriel_Brun.docx'), buf);
-    fs.writeFileSync(path.resolve(__dirname, 'CV_Anonyme.docx'), buf2);
-  }
-);
+    fs.writeFileSync(path.resolve(__dirname, output), buf);
+  });
+}
+
+const markdownCV =
+  '/home/lyd/APPS-SCRIPTS/190907-developpeur-react-nord/src/markdown-pages/cv.md';
+
+makeResume(markdownCV, 'cvTemplate.docx', 'CV_Gabriel_Brun.docx');
+
+makeResume(markdownCV, 'anonymeTemplate.docx', 'CV_Anonyme.docx', true);
